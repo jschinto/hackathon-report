4a6e5459b041668d9e3bf170938f9f3e47a42d5b
Trent Sawicki <trentsawicki@gmail.com>
2018-11-03 13:23:20 -0400

    userdata update

--- userdata.js
+++ userdata.js
@@ -1,20 +1,25 @@
 function userdata() {
+  //access level 1
   var user1 = "Employee 1 (Complainant)";
   const user1key = "ALTR-55A0A8E9B08559CDAAEB918987A1A15B";
   const user1secret = "1385e7c77eae94a37698e42c81318f6fc30c2d45f9abdda8096074c56eaedf33";
 
+  //access level 1
   var user2 = "Employee 2 (Accused)";
   const user2key = "ALTR-470FFDC3CCB254AAAEB0025281BCD317";
   const user2secret = "3fa6b6002921f07f78604b0ef5c61102ee94139a6096eb12f3e8f05adc1f827e";
 
+  //access level 2
   var user3 = "Manager";
   const user3key = "ALTR-F6423DB73F915A07859E2ACAB560D566";
   const user3secret = "2b9b335c05dd03f2e184dbb2a6da212f3d2e90c459e3a521def0d989c73ecad2";
 
+  //access level 3
   var user4 = "HR Rep";
   const user4key = "ALTR-D0438DB1862C5DBDBD502C8D4688C20F";
   const user4secret = "270b16aaaeed2fa7c06aa6befcadcb501199ea7413a174341da4efa14e6c6b70";
 
+  //access level 3
   var user5 = "Executive";
   const user5key = "ALTR-7F531BF453835B4280FBEB6A050711A0";
   const user5secret = "9d7352d641ba7cb2c235adb9b7de4cc0bdb650b30cf1ac91d34ce19c15d098e5";
