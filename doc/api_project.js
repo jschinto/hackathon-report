d0e94303148151071fda043e1efdf64f468aded8
jschinto98 <jakeschinto@gmail.com>
2018-11-03 09:31:48 -0400

    adding the starter stuff

---
+++ doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "chainAPI",
+  "version": "1.0.0",
+  "description": "chainAPI documentation",
+  "title": "chainAPI documentation",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-10-19T16:32:18.105Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
---
+++ main.js
@@ -0,0 +1,3 @@
+//This will be run first
+
+console.log("running");
